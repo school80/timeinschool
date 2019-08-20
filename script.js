d2c6070b8f6bbdd650e5a0c1beaf0da5e5463726
school80 <54249032+school80@users.noreply.github.com>
2019-08-20 12:37:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+let dates = {
+    convert:function(d) {
+        // Converts the date in d to a date-object. The input can be:
+        //   a date object: returned without modification
+        //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+        //   a number     : Interpreted as number of milliseconds
+        //                  since 1 Jan 1970 (a timestamp)
+        //   a string     : Any format supported by the javascript engine, like
+        //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+        //  an object     : Interpreted as an object with year, month and date
+        //                  attributes.  **NOTE** month is 0-11.
+        return (
+            d.constructor === Date ? d :
+                d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+                    d.constructor === Number ? new Date(d) :
+                        d.constructor === String ? new Date(d) :
+                            typeof d === "object" ? new Date(d.year,d.month,d.date) :
+                                NaN
+        );
+    },
+    compare:function(a,b) {
+        // Compare two dates (could be of any type supported by the convert
+        // function above) and returns:
+        //  -1 : if a < b
+        //   0 : if a = b
+        //   1 : if a > b
+        // NaN : if a or b is an illegal date
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+            isFinite(a=this.convert(a).valueOf()) &&
+            isFinite(b=this.convert(b).valueOf()) ?
+                (a>b)-(a<b) :
+                NaN
+        );
+    },
+    inRange:function(d,start,end) {
+        // Checks if date in d is between dates in start and end.
+        // Returns a boolean or NaN:
+        //    true  : if d is between start and end (inclusive)
+        //    false : if d is before start or after end
+        //    NaN   : if one or more of the dates is illegal.
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+            isFinite(d=this.convert(d).valueOf()) &&
+            isFinite(start=this.convert(start).valueOf()) &&
+            isFinite(end=this.convert(end).valueOf()) ?
+                start <= d && d <= end :
+                NaN
+        );
+    }
+};
+
 function timer(_time, _call){
     timer.lastCall = _call;
     timer.lastTime = _time;
@@ -31,188 +83,60 @@ timer.start = function(){
 let root = document.getElementById('root');
 let lessons = [];
 
-for (let i=0; i<=13; i++){
-    lessons[i] = new Date();
+function newDates(){
+    for (let i=0; i<=13; i++){
+        lessons[i] = new Date();
+    }
+
+    lessons[0].setHours(9,0,0);
+    lessons[2].setHours(9,55,0);
+    lessons[4].setHours(11,0,0);
+    lessons[6].setHours(12,5,0);
+    lessons[8].setHours(13,10,0);
+    lessons[10].setHours(14,5,0);
+    lessons[12].setHours(15,0,0);
+
+    lessons[1].setHours(9,45,0);
+    lessons[3].setHours(10,40,0);
+    lessons[5].setHours(11,45,0);
+    lessons[7].setHours(12,50,0);
+    lessons[9].setHours(13,55,0);
+    lessons[11].setHours(14,50,0);
+    lessons[13].setHours(15,45,0);
 }
 
-lessons[0].setHours(9,0,0);
-lessons[2].setHours(9,55,0);
-lessons[4].setHours(11,0,0);
-lessons[6].setHours(12,5,0);
-lessons[8].setHours(13,10,0);
-lessons[10].setHours(14,5,0);
-lessons[12].setHours(15,0,0);
+function start() {
+    timer.pause();
+    let now = new Date();
 
-lessons[1].setHours(9,45,0);
-lessons[3].setHours(10,40,0);
-lessons[5].setHours(11,45,0);
-lessons[7].setHours(12,50,0);
-lessons[9].setHours(13,55,0);
-lessons[11].setHours(14,50,0);
-lessons[13].setHours(15,45,0);
+    for(let i=0; i<13; i++){
+        console.log('test');
+        if (dates.inRange(now, lessons[i], lessons[i+1])) {
+            console.log(i);
+            let between = lessons[i+1] - now;
+            let minutes = Math.trunc(between/60/1000);
+            let seconds = Math.trunc((between/1000)%60);
 
-let today = new Date();
-today.setHours(9,0,0);
+            timer([0,minutes,seconds], function(h,m,s){
+                if (i % 2 == 0){
+                    root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
+                } else {
+                    root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
+                }
+            });
 
-function isStart(){
-    console.log('test');
-    if ((new Date() <= today) || (new Date() >= lessons[13])){
+            timer.pause();
+            timer.start();
+
+            setTimeout(() => start(), between);
+        }
+    }
+    if (now >= lessons[13] || now <= lessons[0]){
         root.innerHTML = 'Уроки закончились';
-        console.log('today: ', today);
-        console.log('new Date:', new Date());
-        setTimeout(() => isStart(),1000);
-    } else {
-        startDay();
+        newDates();
+        setTimeout(() => start(), 1000);
     }
 }
 
-function startDay() {
-    console.log('testt');
-    //1 urok
-    timer([0,44,59],
-        function (h,m,s) {
-            root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-        })
-    timer.pause();
-    timer.start();
-
-    //1 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,9,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[1]-lessons[0]);
-
-    //2 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[2]-lessons[0]);
-
-    //2 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,19,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[3]-lessons[0]);
-
-    //3 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[4]-lessons[0]);
-
-    //3 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,19,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[5]-lessons[0]);
-
-    //4 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[6]-lessons[0]);
-
-    //4 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,19,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[7]-lessons[0]);
-
-    //5 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[8]-lessons[0]);
-
-    //5 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,9,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[9]-lessons[0]);
-
-    //6 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[10]-lessons[0]);
-
-    //6 peremena
-    setTimeout(function(){
-        timer.pause();
-        timer([0,9,59],
-            function (h,m,s) {
-                root.innerHTML = 'До начала урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[11]-lessons[0]);
-
-    //7 urok
-    setTimeout(function(){
-        timer.pause();
-        timer([0,44,59],
-            function (h,m,s) {
-                root.innerHTML = 'До конца урока осталось: ' + m + ':' + s;
-            })
-        timer.pause();
-        timer.start();
-    },lessons[12]-lessons[0]);
-
-    setTimeout(function(){
-        timer.pause();
-        isStart();
-    },lessons[13]-lessons[0]);
-}
-
-
-isStart();
+newDates();
+start();
